Fixed the pressure calculation. Somehow not the same pressure is achieved.

main.py
@@ -1,5 +1,6 @@
+import timing
 import numpy as np
-from scipy.constants import k, u, value, nano
+from scipy.constants import k, u, value, milli
 from experiment import Experiment
 from domain import Cuboid
 
@@ -17,6 +18,7 @@ T = 300 # Kelvin
 v = np.sqrt(2 * k * T / m)
 
 print("Initial energy:", str(N * k * T), "J")
+print("Initial speed:", str(v))
 
 cube = Cuboid([l for dim in range(f)])
 
@@ -24,7 +26,7 @@ def speedFunction(): return v
 
 print("Ideal pressure:", str(N * k * T / cube.setVolume()), "J/m**3") # N = 100, T = 300 K, V = 1 m**3, p = N * k * T / V
 
-exp = Experiment(cube, Experiment.createParticleList(N, cube, speedFunction, m, r), nano)
+exp = Experiment(cube, Experiment.createParticleList(N, cube, speedFunction, m, r), milli)
 exp.showState()
-exp.runStep(1000)
+exp.runStep(5000)
 exp.showState()